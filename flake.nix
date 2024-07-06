ccd260d5d562e28aa86a66b442738e5bad3b5488
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2024-07-06 16:10:02 -0500

    build: introduce nix flake

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    libpisp-src = {
+      flake = false;
+      url = "github:raspberrypi/libpisp/v1.0.6";
+    };
+  };
+
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux" "aarch64-linux"];
+      perSystem = {
+        config,
+        pkgs,
+        system,
+        inputs',
+        self',
+        ...
+      }: let
+        libpisp = pkgs.stdenv.mkDerivation {
+          name = "libpisp";
+          version = "1.0.3";
+          src = inputs.libpisp-src;
+          nativeBuildInputs = [pkgs.pkg-config pkgs.meson pkgs.ninja];
+          buildInputs = [pkgs.nlohmann_json pkgs.boost];
+          # Meson is no longer able to pick up Boost automatically.
+          # https://github.com/NixOS/nixpkgs/issues/86131
+          BOOST_INCLUDEDIR = "${pkgs.lib.getDev pkgs.boost}/include";
+          BOOST_LIBRARYDIR = "${pkgs.lib.getLib pkgs.boost}/lib";
+        };
+      in {
+        packages.libcamera = pkgs.libcamera.overrideAttrs (old: {
+          nativeBuildInputs = old.nativeBuildInputs ++ [pkgs.python3Packages.pybind11];
+          buildInputs = old.buildInputs ++ [libpisp];
+          postPatch = ''
+            patchShebangs src/py/ utils/
+          '';
+          src = ./.;
+        });
+      };
+    };
+}
